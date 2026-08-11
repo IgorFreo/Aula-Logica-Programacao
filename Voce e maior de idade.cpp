8576f9ac158377e58ddc57b7f4db863a3c698c25
Igor Mateus Fréo <igormateusfreo5@gmail.com>
2026-08-11 19:36:09 -0300

    Add files via upload

---
+++ Voce e maior de idade.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main (){
+	
+	int idade;
+	
+	printf("Digite a sua idade");
+	scanf("%d", &idade);
+	
+	if (idade >= 18){
+		
+		printf("Voce e maior de idade \n ");
+		
+	}else{
+		
+		printf("Voce e menor de idade");
+		
+	}
+	
+	return 0;
+}
