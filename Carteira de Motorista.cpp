880aac2c9d10dc5ac143ac1862ca10ca8e1ce46e
Igor Mateus Fréo <igormateusfreo5@gmail.com>
2026-08-18 19:36:21 -0300

    Add files via upload

---
+++ Carteira de Motorista.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>//Biblioteca
+#include <stdbool.h>//Biblioteca verdadeira ou falso
+
+int main(){//Classe Principal
+	int idade = 20;//Declarando uma variavel
+	bool tem_carteira = true;//Condição verdadeira
+	
+	if (idade >= 18){//Maior ou menor de idade
+		printf("Maior de idade.\n");// Mostra a mensagem na tela
+		
+		if ("tem_carteira.\n"){// Se a pessoa tem carteira
+			printf("Voce pode dirigir. \n");// Mostra a mensagem na tela
+			
+			
+		}else{// Senão
+			printf("Vece não tem carteira de motorista. \n");// Mostra a mensagem na tela
+			
+		}
+		
+	}else{
+		printf("Menor de idade. \n");// Mostra a mensagem na tela
+	}	
+		return 0;// Fim do Programa
+	
+	
+}
